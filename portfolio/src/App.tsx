8c6a25797ef90bbb3c76975ec3c48282ab6bec0a
r3dc0g <critesg@spu.edu>
2024-02-27 12:45:32 -0800

    Finished Project Section, moved to Contact

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,8 +1,10 @@
 import styles from './App.module.css'
 import {Hero} from './components/Hero/Hero';
-import { Navbar } from "./components/Navbar/Navbar";
-import { About } from "./components/About/About";
+import {Navbar} from "./components/Navbar/Navbar";
+import {About} from "./components/About/About";
 import {Experience} from './components/Experience/Experience';
+import {Projects} from './components/Projects/Projects';
+import {Contact} from './components/Contact/Contact';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
         <Hero />
         <About />
         <Experience />
+        <Projects />
+        <Contact />
     </div>
   )
 }
---
+++ portfolio/src/components/Contact/Contact.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+    return (
+        <section>Contact</section>
+    );
+};
